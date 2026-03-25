f6107fb2ccda52d071d9f09ebb5400fc9cd254bf
KacperOrkwiszewski <251197@edu.p.lodz.pl>
2026-03-25 19:18:40 +0100

    immplement addition interface trnaslated to python

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 print("PoleHipNeed SCRUM Master, DevOps KWojtal-lab")
 print("PoleHipNeed Developer KacperOrkwiszewski")
 print("PoleHipNeed Developer mechanikal")
+
+def addition(a, b):
+    """Implements addition interface."""
+    return a + b
