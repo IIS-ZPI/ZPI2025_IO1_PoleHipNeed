ea565f73a1a38706cd31d1c7f90a9bf595fe7fd5
mechanikal <251160@edu.p.lodz.pl>
2026-03-25 19:22:49 +0100

    add multiplication feature to app

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 print("PoleHipNeed SCRUM Master, DevOps KWojtal-lab")
 print("PoleHipNeed Developer KacperOrkwiszewski")
 print("PoleHipNeed Developer mechanikal")
+
+def multiplication(a, b):
+    return a * b
