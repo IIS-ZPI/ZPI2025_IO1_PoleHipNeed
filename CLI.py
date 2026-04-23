a84ad5b5f2cfb193003062788f0deb30d51a167d
mechanikal <251160@edu.p.lodz.pl>
2026-04-23 11:26:37 +0200

    Implement currency select method

--- CLI.py
+++ CLI.py
@@ -1,4 +1,4 @@
-from enum import IntEnum
+from enum import IntEnum, Enum
 from colorama import Fore
 
 
@@ -11,10 +11,61 @@ class AnalysisType(IntEnum):
     def has_value(cls, value):
         return value in cls._value2member_map_
 
+class Currency(Enum):
+    THAI_BAHT = 'THB'
+    US_DOLLAR = 'USD'
+    AUSTRALIAN_DOLLAR = 'AUD'
+    HONG_KONG_DOLLAR = 'HKD'
+    CANADIAN_DOLLAR = 'CAD'
+    NEW_ZEALAND_DOLLAR = 'NZD'
+    SINGAPORE_DOLLAR = 'SGD'
+    EURO = 'EUR'
+    HUNGARIAN_FORINT = 'HUF'
+    SWISS_FRANC = 'CHF'
+    BRITISH_POUND = 'GBP'
+    UKRAINIAN_HRYVNIA = 'UAH'
+    JAPANESE_YEN = 'JPY'
+    CZECH_KORUNA = 'CZK'
+    DANISH_KRONE = 'DKK'
+    ICELANDIC_KRONA = 'ISK'
+    NORWEGIAN_KRONE = 'NOK'
+    SWEDISH_KRONA = 'SEK'
+    ROMANIAN_LEU = 'RON'
+    TURKISH_LIRA = 'TRY'
+    ISRAELI_SHEKEL = 'ILS'
+    CHILEAN_PESO = 'CLP'
+    PHILIPPINE_PESO = 'PHP'
+    MEXICAN_PESO = 'MXN'
+    SOUTH_AFRICAN_RAND = 'ZAR'
+    BRAZILIAN_REAL = 'BRL'
+    MALAYSIAN_RINGGIT = 'MYR'
+    INDONESIAN_RUPIAH = 'IDR'
+    INDIAN_RUPEE = 'INR'
+    SOUTH_KOREAN_WON = 'KRW'
+    CHINESE_YUAN = 'CNY'
+    SPECIAL_DRAWING_RIGHTS = 'XDR'
+    POLISH_ZLOTY = 'PLN'
+
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_
+
+    @classmethod
+    def to_string(cls, divider):
+        string = ''
+        first = True
+        for c in cls._value2member_map_:
+            if not first:
+                string += divider
+            string += c
+            first = False
+        return string
 
 class CLI:
     def __init__(self):
         self.selected_analysis = None
+        self.selected_currency = None
+
         print(Fore.CYAN+"NBP Data Analysis Tool by PoleHipNeed",Fore.RESET)
 
     def my_print(self, message_type, *args):
@@ -36,7 +87,7 @@ class CLI:
         return input()
 
 
-    def aquire_analysis_type(self):
+    def acquire_analysis_type(self):
         selected_analysis = None
 
         self.my_print('default', "select type of statistical analysis\n")
@@ -48,25 +99,42 @@ class CLI:
 
         if len(user_input) != 1:
             self.my_print('error', "\nINPUT INVALID (only one character allowed)\n")
-            self.aquire_analysis_type()
+            self.acquire_analysis_type()
             return
 
         try:
             selected_analysis = int(user_input)
         except ValueError:
             self.my_print('error', "\nINPUT INVALID (please enter a number)\n")
-            self.aquire_analysis_type()
+            self.acquire_analysis_type()
             return
 
         if  not AnalysisType.has_value(selected_analysis):
             self.my_print('error', "\nINPUT INVALID (number not in available types)\n")
-            self.aquire_analysis_type()
+            self.acquire_analysis_type()
         else:
             self.selected_analysis = AnalysisType(selected_analysis)
-            self.my_print('success', '\nselected: ', cli.selected_analysis.name)
+            self.my_print('success', '\nselected: ', cli.selected_analysis.name.lower().replace('_', ' '))
+
+
+    def acquire_currency(self):
+        selected_currency = None
+
+        self.my_print('default', "select currency for analysis by inputting its code\n")
+        self.my_print('info', 'available currencies:\n', Currency.to_string(', '),"\n")
+
+        selected_currency = self.get_input().upper()
+
+        if not Currency.has_value(selected_currency):
+            self.my_print('error', "\nINPUT INVALID (currency code not recognized)\n")
+            self.acquire_currency()
+        else:
+            self.selected_currency = Currency(selected_currency)
+            self.my_print('success', '\nselected: ', cli.selected_currency.name.lower().replace('_', ' '))
 
 
 if __name__ == "__main__":
     cli = CLI()
-    cli.aquire_analysis_type()
+    cli.acquire_currency()
+    cli.acquire_analysis_type()
 
